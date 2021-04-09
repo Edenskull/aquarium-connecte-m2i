e7576e73be447c194a00644c86a43217a6cd4068
EdenSkull <maxchamp@live.fr>
2021-04-09 17:27:47 +0200

    add card template + graph template

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ include('templates/core/head.php');
 ?>
 
 <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     <?php
 
@@ -24,9 +27,6 @@ include('templates/core/head.php');
 
     ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- templates/dashboard/dashboard.php
+++ templates/dashboard/dashboard.php
@@ -1,7 +1,73 @@
 <div id="corps" class="container-fluid">
     <div class="row">
-        <nav class="sidebar col-md-4 col-lg-2 d-md-block bg-dark">
+        <nav class="sidebar col-md-3 col-lg-2 d-md-block bg-dark collapse">
 
         </nav>
+        <main class="col-md-9 ms-sm-auto col-lg-10 p-4">
+            <div class="row">
+                <div class="col-xl-2 col-md-2 col-sm-4 col-12">
+                    <div class="col-12">
+                        <div class="card text-light bg-info mb-3">
+                            <div class="card-header">Température actuelle</div>
+                            <div class="card-body">
+                                <div class="media d-flex">
+                                    <div class="align-self-center">
+                                        <i class="card-icon bi bi-thermometer-half float-right"></i>
+                                    </div>
+                                    <div class="media-body text-end">
+                                        <h2>20°</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-10 col-md-10 col-sm-8 col-12">
+                    <div class="card text-light bg-light">
+                        <div class="card-body">
+                            <canvas id="chart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </div>
+
+<script>
+    var ctx = $("#chart").get(0).getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
